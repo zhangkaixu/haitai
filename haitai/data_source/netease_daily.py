1b6eaa1d517400951c47a96c9f0415746c5f8d23
zhangkaixu <zhangkaixu@hotmail.com>
2014-02-25 17:24:29 +0800

    增加data_source

---
+++ haitai/data_source/netease_daily.py
@@ -0,0 +1,39 @@
+import time
+import haitai
+import os
+def get_today(stock_file):
+    if os.path.exists(stock_file) :
+        for k,l in enumerate(open(stock_file)):
+            if k == 1 :
+                l=l.split(',')[0]
+                return l
+    return None
+
+def refresh_stock(stock_id,today):
+    """
+    如果最新日期不是today,则更新 stock_id 这只股票的记录
+    """
+    url="""http://quotes.money.163.com/service/chddata.html?code=%(a)s&end=%(today)s&fields=TCLOSE;HIGH;LOW;TOPEN;LCLOSE;CHG;PCHG;TURNOVER;VOTURNOVER;VATURNOVER;TCAP;MCAP"""
+    dest=haitai.daily_dir
+    stock_id,_,mkt=stock_id.partition('.')
+    if mkt=='sz' :
+        stock_id='1'+stock_id
+    else :
+        stock_id='0'+stock_id
+
+    stock_file=stock_id[1:]+'.'+('ss' if stock_id[0]=='0' else 'sz')
+    stock_file=os.path.join(dest,stock_file)
+
+    last=get_today(stock_file)
+    if last and ''.join(last.split('-'))==today : 
+        #print("%s alread today"%(stock_id))
+        return None
+
+    cmd=('wget "'+url+'''" -q -O - | iconv -f gbk >  %(f)s''')%{
+            'a': stock_id,'f': stock_file,
+            'today': today}
+    time.sleep(1) # sleep for not being blocked
+    os.system(cmd)
+
+    return get_today(stock_file)
+    
--- haitai/scripts/163.py
+++ haitai/scripts/163.py
@@ -6,6 +6,7 @@ import random
 import datetime
 
 import haitai
+from haitai.data_source import netease_daily
 
 help_text="""使用163下载股票每日历史数据
 
@@ -17,14 +18,6 @@ help_text="""使用163下载股票每日历史数据
     ./haitai/scripts/163.py 2013-11-01 stocks # 下载所有股票信息"""
 
 url="""http://quotes.money.163.com/service/chddata.html?code=%(a)s&end=%(today)s&fields=TCLOSE;HIGH;LOW;TOPEN;LCLOSE;CHG;PCHG;TURNOVER;VOTURNOVER;VATURNOVER;TCAP;MCAP"""
-
-def get_today(stock_file):
-    if os.path.exists(stock_file) :
-        for k,l in enumerate(open(stock_file)):
-            if k == 1 :
-                l=l.split(',')[0]
-                return l
-    return None
 
 
 if __name__ == '__main__':
@@ -56,8 +49,6 @@ if __name__ == '__main__':
 
     dest=haitai.daily_dir
 
-    
-
     print("下载截止日期为 %s 的 %s 只股票每日行情到目标 %s" %(today,len(stock_ids),dest))
     stock_ids=sorted(stock_ids)
 
@@ -67,34 +58,9 @@ if __name__ == '__main__':
     os.system('mkdir -p '+os.path.join(dest))
 
     for i,stock_id in enumerate(stock_ids) :
-
-        stock_id,_,mkt=stock_id.partition('.')
-        if mkt=='sz' :
-            stock_id='1'+stock_id
-        else :
-            stock_id='0'+stock_id
-
-        stock_file=stock_id[1:]+'.'+('ss' if stock_id[0]=='0' else 'sz')
-        stock_file=os.path.join(dest,stock_file)
-
-        last=get_today(stock_file)
-        if last and ''.join(last.split('-'))==today : 
-            #print("%s alread today"%(stock_id))
-            continue
-
-
-        cmd=('wget "'+url+'''" -q -O - | iconv -f gbk >  %(f)s''')%{
-                'a':stock_id,'f': stock_file,
-                'today': today}
-        if i+1 !=len(stock_ids) :
-            #time.sleep(1+random.random()*3)
-            time.sleep(1)
-        #print(cmd)
-        os.system(cmd)
-
-        last=get_today(stock_file)
+        last=netease_daily.refresh_stock(stock_id,today)
+        if not last : continue
         now=time.time()
         sec_left=(now-start_time)/(i+1)*(n-i-1)
         print(stock_id,last)
         print("%.2f%% %.2f"%((i+1)/n*100,sec_left),end='\r')
-
